979d47da16ce4be4d01e83e1548c0d0a4dd653f8
Asish <asish.panda13@gmail.com>
2021-10-25 17:56:34 +0400

    Completed database layer part 1

---
+++ BookStore.data/Interfaces/IBookRepo.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.data.Interfaces
+{
+    public interface IBookRepo
+    {
+        List<Book> GetAllBooks();
+        Book GetBook(int id);
+    }
+}
--- WebApplication1/Controllers/BooksController.cs
+++ WebApplication1/Controllers/BooksController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 //using BookstoreAPI.Models;
 using BookStore.data.Models;
+using BookStore.data.Interfaces;
+using BookStore.data.Repo;
 
 namespace BookstoreAPI.Controllers
 {
@@ -13,31 +15,21 @@ namespace BookstoreAPI.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        public List<Book> books = new()
-        {
-            new Book { Id = 1, Title = "The Girl on the Train", Author = "Hawkins, Paula", PublicationYear = 2015, CallNumber = "F HAWKI" },
-            new Book { Id = 2, Title = "Rogue Lawyer", Author = "Grisham, John", PublicationYear = 2015, CallNumber = "F GRISH" },
-            new Book { Id = 3, Title = "After You", Author = "Moyes, Jojo", PublicationYear = 2015, CallNumber = "F MOYES" },
-            new Book { Id = 4, Title = "All the Light We Cannot See", Author = "Doerr, Anthony", PublicationYear = 2014, CallNumber = "F DOERR" },
-            new Book { Id = 5, Title = "The Girls", Author = "Cline, Emma", PublicationYear = 2016, CallNumber = "F CLINE" },
-            new Book { Id = 6, Title = "The Martian", Author = "Weir, Andy", PublicationYear = 2011, CallNumber = "SF WEIR" },
-            new Book { Id = 7, Title = "Me Before You", Author = "Moyes, Jojo", PublicationYear = 2012, CallNumber = "F MOYES" },
-            new Book { Id = 8, Title = "Alexander Hamilton", Author = "Chernow, Ron", PublicationYear = 2004, CallNumber = "B HAMILTO A" },
-            new Book { Id = 9, Title = "Before the Fall", Author = "Hawley, Noah", PublicationYear = 2016, CallNumber = "F HAWLE" }
-        };
 
+        private IBookRepo books = new BookRepo();
         [HttpGet]
 
         public ActionResult<IEnumerable<Book>> GetAllBooks()
         {
-            return books;
+            return books.GetAllBooks();
         }
 
         [HttpGet("{id}")]
 
         public ActionResult<Book> GetBook(int id)
         {
-            var book =  books.FirstOrDefault(x => x.Id == id);
+            //var book =  books.FirstOrDefault(x => x.Id == id);
+            var book = books.GetBook(id);
             if (book == null)
             {
                 return NotFound();
